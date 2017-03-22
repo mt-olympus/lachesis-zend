0d0537a2e8d7c74de6e27a3cc3e7014188cd1e26
Leandro Silva <leandro.silva@zoox.com.br>
2017-03-22 10:47:34 -0300

    Adding middleware for request injection

---
+++ src/LachesisMiddlewareFactory.php
@@ -0,0 +1,26 @@
+<?php
+namespace Kharon\Hermes;
+
+use Interop\Container\ContainerInterface;
+use Zend\ServiceManager\FactoryInterface;
+use Zend\ServiceManager\ServiceLocatorInterface;
+
+class LachesisMiddlewareFactory implements FactoryInterface
+{
+
+    public function __invoke(ContainerInterface $container, $requestedName, array $options = null)
+    {
+        $collector = $container->get(Collector::class);
+
+        return new CollectorMiddleware($collector);
+    }
+
+    /**
+     * {@inheritDoc}
+     * @see \Zend\ServiceManager\FactoryInterface::createService()
+     */
+    public function createService(ServiceLocatorInterface $serviceLocator) {
+        return $this->__invoke($serviceLocator, CollectorMiddleware::class);
+    }
+
+}
